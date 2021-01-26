cbd4fa4134613975ceb5f5d24e96be128c231087
Matthew Chute <mchute@pm.me>
2021-01-26 13:05:12 -0800

    add tcp

---
+++ tcp/README.md
@@ -0,0 +1,4 @@
+## Purpose   
+Use TCP to transfer a file from a client to a server.   
+
+## Running the code:  
---
+++ tcp/src/constants.py
@@ -0,0 +1,4 @@
+BUFFER_SIZE = 1024
+EOF = b'<EOF>'
+SERVER_FILE_PATH = '../assets/server'
+CLIENT_FILE_PATH = '../assets/client'
